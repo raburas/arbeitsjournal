800e287b2ed4e4bb646f7ced9c5f10081d08f481
AburasRustom <AburasRustom@RABURASDT-W10>
2016-08-15 11:44:57 +0200

    Add project files.

---
+++ ArbeitsJournal.Client/app/test.js
@@ -0,0 +1,20 @@
+﻿var xmlhttp = new XMLHttpRequest();
+var serverUrl = "http://localhost:3364/rest/api";
+
+
+function httpGetAsync(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    var serviceUrl = serverUrl + url;
+    console.log("serviceUrl: ", serverUrl);
+    xmlHttp.open("GET", serviceUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+};
+
+function setResultat(resultat) {
+    document.getElementById("resultat").innerHTML = resultat;
+    console.log("resultat: ", resultat);
+};
---
+++ ArbeitsJournal.Service/Controllers/JournalController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace ArbeitsJournal.Service.Controllers
+{
+    public class JournalController : ApiController
+    {
+
+        [HttpGet]
+        public string GetHelloWorld() {
+            return "Hello world from your server";
+        }
+    }
+}
